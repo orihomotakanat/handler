2984cf440d6029566c64cef56acc3826c22edaee
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-08-14 11:37:21 +0900

    Add new function

--- bin/mkcase.sh
+++ bin/mkcase.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-#$1 -> number
-#$2 -> field
-
 date=$(date +"%Y%m%d")
-filename=$date-$1-$2.txt
+filename=$date-$1-$2.txt #$1 -> number, #$2 -> field
 
-`atom $filename`
+if [ -z "$1" -o -z "$2" ]; then #$1, $2がnullの場合
+  echo "\$ mkcs [number] [field]"
+  exit 1
+elif [ -e "$filename" ]; then
+  echo "$filename already exists."
+  exit 1
+else
+  echo "Make new file $filename"
+  `atom $filename`
+fi
