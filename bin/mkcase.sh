742263eb6790c18282bd90d22aad45953d27e931
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-10-25 00:53:24 +0900

    Modification of new option

--- bin/mkcase.sh
+++ bin/mkcase.sh
@@ -11,10 +11,15 @@ if [ -z "$1" -o -z "$2" ]; then #$1, $2がnullの場合
 elif [ -e "$txtfile" ]; then
   echo "$txtfile already exists."
   exit 1
-elif [ "$3"=~"\-txt" ]; then
-  echo "Make new file $txtfile"
+elif [ -n "$3" ]; then #$3がnotNullかつ-txtの場合
+  if [ "$3" != "-txt" ]; then
+    echo "Invalid option"
+    echo "[option] -txt: make text file"
+    exit 1
+  fi
+  echo "Make new text file $txtfile"
   `atom $txtfile`
 else
-  echo "Make new file $markdownfile"
+  echo "Make new git-markdown file $markdownfile"
   `atom $markdownfile`
 fi
